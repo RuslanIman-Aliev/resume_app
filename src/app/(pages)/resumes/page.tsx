a8083eaf4fab6e95ec583635c3538661763608de
RuslanIman-Aliev <s0970802047@gmail.com>
2026-03-21 20:01:32 +0100

    feat: implement resume management features with upload functionality and UI components

---
+++ src/app/(pages)/resumes/page.tsx
@@ -0,0 +1,13 @@
+import ResumeCard from "@/features/resumes/components/resume-cart";
+import ResumeManager from "@/features/resumes/components/resume-manager";
+
+const ResumesPage = () => {
+  return (
+    <div className="flex flex-col container max-w-7xl mx-auto pt-10">
+      <ResumeManager />
+      <ResumeCard />
+    </div>
+  );
+};
+
+export default ResumesPage;
